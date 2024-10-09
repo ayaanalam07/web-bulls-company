b575f48d3ee90c9297c6f8040d78b6044d6512bd
ayaanalam07 <ayaanabaf@gmail.com>
2024-10-09 14:39:48 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,12 @@
+ AOS.init();
+    
+    // Subscribe Section Animation
+    window.addEventListener('scroll', function() {
+        const subscribeSection = document.getElementById('subscribe-section');
+        const sectionPosition = subscribeSection.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight;
+
+        if (sectionPosition < screenPosition) {
+            subscribeSection.classList.add('appear');
+        }
+    });
